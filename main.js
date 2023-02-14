494a1e73ba05d21949c226a5bad93f30773a874c
alanniedecena <alannie.khem.decena@gmail.com>
2023-02-14 20:19:01 +1300

    js.ex1

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.getElementById("demo").className = "red";
 
+console.log('console message')
+
 function helloWorld(){
     alert('Hello World!');
+    console.info(document.getElementById('helloWorldButton'))
     document.getElementById("helloWorldButton").style.backgroundColor = 'black';
     document.getElementById("helloWorldButton").style.color = 'white';
 }
