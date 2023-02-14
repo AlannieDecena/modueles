60665008fadf6e86f3b046186a5eceb9700f2b94
alanniedecena <alannie.khem.decena@gmail.com>
2023-02-14 16:11:02 +1300

    ex.2

--- Exercise_2/helloWorld.js
+++ Exercise_2/helloWorld.js
@@ -1,1 +1,33 @@
 console.log('hello world')
+
+
+function SayHello(){
+console.log('Hello again')
+
+}
+
+SayHello()
+
+function sum(a, b) {
+    let result = a+b;
+    return result;
+}
+
+console.log(sum(88, 44))
+
+console.log(sum(123, 456))
+
+SayHello()
+
+var Sentiment = require('sentiment');
+
+function analyzeSentence(sentence) {
+    var sentiment = new Sentiment();
+    var result = sentiment.analyze(sentence);
+    return result.score;
+}
+
+console.log('Your score is' + analyzeSentence('I love chocos')); 
+
+
+
