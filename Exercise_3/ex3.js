fb456bdb137eb0cffd4f477453fb063736d5de88
alanniedecena <alannie.khem.decena@gmail.com>
2023-02-15 13:43:59 +1300

    ex3.day3

--- Exercise_3/ex3.js
+++ Exercise_3/ex3.js
@@ -1,3 +1,47 @@
-console.log('hello')
 
-console.log('testing')
+
+//console.log('testing');
+
+//console.log('hi');
+
+//function example() {
+  // let exampleVar = 1;
+
+//let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+//console.log(mountains[1])
+//console.log(mountains[2])
+//console.log(mountains[0])
+
+//mountains.push('K2')
+//console.log(mountains)
+
+//let lastMountain = mountains.pop ()
+//console.log('last Mountain is' + lastMountain)
+
+//let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+//console.log(seas);
+//seas.push('Red Sea');
+//seas.unshift('Adriatic Sea')
+//console.log(seas);
+
+//let firstSea = seas.shift()
+//console.log('first sea is' + firstSea)
+
+//let shoppingList = ['shoes', 'dress', 'bag', 'pants', 'glasses'];
+//shoppingList ['1', '4'] = 'paper', 'pen';
+//console.log(shoppingList);
+
+
+//const user = {
+  //  "first_name" : "Sammy",
+    //"last_name" : "Shark",
+    //"age" : 25,
+    //"followers" : 987
+    //}
+    //console.log (user.first_name + ' is ' + user.age + 'years old');
+    //console.log(user.age)
+
+    //function sun (a, b) {
+      //  return a+b;
+   // }
+  //  If (sum(2,4)!=6) { throw new error} 
